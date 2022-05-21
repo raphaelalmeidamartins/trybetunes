6794705f33a865251e8cd955bbb5a4c2a2d34b17
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-21 13:45:54 -0300

    :recycle: refactor: change the removed the as in the BrowserRouter component import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
