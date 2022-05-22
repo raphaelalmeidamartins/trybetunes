9e654689c427811bd901f624edd5f3daffabc691
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-22 16:00:48 -0300

    :construction: build: add mui and emotion dependencies

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
+      <CssBaseline />
       <App />
     </HashRouter>
   </React.StrictMode>,
